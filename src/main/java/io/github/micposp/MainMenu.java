a4a5159108b0787e3796b2135e4d989e0eb2ca73
Mikhail <micposp@gmail.com>
2019-03-27 15:20:37 +0300

    Make choice

--- src/main/java/io/github/micposp/MainMenu.java
+++ src/main/java/io/github/micposp/MainMenu.java
@@ -1,6 +1,7 @@
 package io.github.micposp;
-import io.github.micposp.*;
-import java.util.Scanner;
+//import io.github.micposp.*;
+//import java.util.Scanner;
+import java.util.*;
 
 public class MainMenu extends AbstractMenu {
 
@@ -17,11 +18,64 @@ public class MainMenu extends AbstractMenu {
 
     @Override
     public void interactWithMenu() {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Choose item: ");
-        while (!in.hasNext()) {
+        int choice=0;
+
+        while (true) {
+            choice=this.getChoice();
+
+            switch (choice) {
+                case 1:
+                    this.Create();
+                    break;
+                case 2:
+                    this.Search();
+                    break;
+                case 3:
+                    this.Sort();
+                    break;
+            }
+
+            this.printMenu();
         }
-        String item = in.next();
-        System.out.println("choice "+item);
+    }
+
+    private int getChoice() {
+        Scanner in = new Scanner(System.in);
+        int choice=0;
+
+        while (true) {
+            System.out.print("Choose item: ");
+
+            try {
+                choice = in.nextInt();
+                if (choice < 1 || choice > this.items.size()) {
+                    throw new MainMenu.InputOutOfBand();
+                }
+                break;
+            }
+            catch (InputMismatchException e) {
+                System.out.println("Incorrect choice format");
+                in.nextLine();
+            }
+            catch (InputOutOfBand e) {
+                System.out.println("Choice is out of bandwidth");
+            }
+        }
+
+        return choice;
+    }
+
+    private static class InputOutOfBand extends Exception { }
+
+    private void Create() {
+
+    }
+
+    private void Search() {
+
+    }
+
+    private void Sort() {
+
     }
 }
