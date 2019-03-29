500975145ac30d1af168d14bd6934f65ea3d1bf0
Mikhail <micposp@gmail.com>
2019-03-29 13:45:16 +0300

    csv parsing implementation

--- src/main/java/io/github/micposp/DbWorker.java
+++ src/main/java/io/github/micposp/DbWorker.java
@@ -1,6 +1,7 @@
 package io.github.micposp;
 import java.io.*;
 import java.util.*;
+import java.text.*;
 
 public class DbWorker {
     private List<Client> clients=null;
@@ -12,9 +13,15 @@ public class DbWorker {
             this.clients = new ArrayList<Client>();
             String line = null;
 
+            line = bufferedReader.readLine();
+
             while ((line = bufferedReader.readLine()) != null) {
-                String[] clientArray=line.split(",");
-                this.clients.add(new Client(clientArray[0],Integer.parseInt(clientArray[1])));
+                String[] clientArray=line.split(";",5);
+                System.out.println(Arrays.toString(clientArray));
+                this.clients.add(new Client(clientArray[0], clientArray[1],
+                                            Integer.parseInt(clientArray[2]),
+                                            Float.parseFloat(clientArray[3]),
+                                            clientArray[4]));
             }
 
             bufferedReader.close();
@@ -53,11 +60,18 @@ public class DbWorker {
 
     public class Client implements Comparable<Client> {
         private String name;
-        private int burnYear;
+        private String burnDate;
+        private int numProjects;
+        private float rating;
+        private String comment;
 
-        public Client(String name, int burnYear) {
+        public Client(String name, String burnDate, int numProjects,
+                      float rating, String comment) {
             this.name=name;
-            this.burnYear=burnYear;
+            this.burnDate=burnDate;
+            this.numProjects=numProjects;
+            this.rating=rating;
+            this.comment=comment;
         }
 
         public String getName() {
@@ -68,21 +82,67 @@ public class DbWorker {
             this.name=name;
         }
 
-        public int getBurnYear() {
-            return this.burnYear;
+        public String getBurnDate() {
+            return this.burnDate;
         }
 
-        public void setBurnYear(int burnYear) {
-            this.burnYear=burnYear;
+        public void setBurnDate(String burnDate) {
+            this.burnDate=burnDate;
+        }
+
+        public int getNumProjects() {
+            return this.numProjects;
+        }
+
+        public void setNumProjects(int numProjects) {
+            this.numProjects=numProjects;
+        }
+
+        public float getRating() {
+            return this.rating;
+        }
+
+        public void setRating(float rating) {
+            this.rating=rating;
+        }
+
+        public String getComment() {
+            return this.comment;
+        }
+
+        public void setComment(String comment) {
+            this.comment=comment;
         }
 
         public int compareTo(Client anotherClient) {
-            return this.burnYear - anotherClient.burnYear;
+            SimpleDateFormat format = new SimpleDateFormat("dd.mm.yyyy");
+            Date dateOne=null;
+            Date dateTwo=null;
+
+            try {
+                dateOne=format.parse(this.burnDate);
+                dateTwo=format.parse(anotherClient.burnDate);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return 0;
+            }
+
+            long delta=dateOne.getTime()-dateTwo.getTime();
+
+            if (delta>0) {
+                return 1;
+            } else if (delta<0) {
+                return -1;
+            } else {
+                return 0;
+            }
         }
 
         @Override
         public String toString(){
-            return "("+this.getName()+", " +this.getBurnYear()+")";
+            return "(" + this.getName() + ", " + this.getBurnDate() + ", " +
+                   this.getNumProjects() + ", " + this.getRating() + ", " +
+                   this.getComment() + ")";
         }
     }
 }
